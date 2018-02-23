03a105d228ab5ba26d58fecf0011bb8dfc207a17
James Reynolds <jameshreynolds@gmail.com>
2018-02-23 11:18:53 -0800

    PledgeInfo component refactored to a stateful component  Please enter the commit message for your changes. Lines starting

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,29 +5,25 @@ import StatelessComponent from './components/StatelessComponent.jsx';
 import PledgeInfo from './components/PledgeInfo.jsx'
 import style from './main.css';
 
-const date = new Date();
+
+const date = new Date(); // temporary work around
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      pledgeInfo: {
-        pledgeTotal: '10,420',
-        pledgeGoal: '7,500',
-        backers: 352,
-        daysToGo: 18,
-        timeStamp: date.toString()
-      }
-    };
+    this.state = {};
   }
   
   render () {
     return (
       <div>
-        <PledgeInfo pledgeInfo={this.state.pledgeInfo}/>
-        {/* <h1> Up and running! </h1>
-        <StatelessComponent />
-        <StatefulComponent /> */}
+        <PledgeInfo info={{
+          pledgeTotal: '10,420',
+          pledgeGoal: '7,500',
+          backers: 352,
+          daysToGo: 18,
+          timeStamp: date.toString()
+        }}/>
       </div>
     )
   }
