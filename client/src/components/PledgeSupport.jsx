0eb69456a3bd9c309891e8ae352f3af52b9632f0
James Reynolds <jameshreynolds@gmail.com>
2018-02-25 15:51:58 -0800

    removed unecessary clutter such as old code that was commented out and a few component files that I do not need Please enter the commit message for your changes. Lines starting

--- client/src/components/PledgeSupport.jsx
+++ client/src/components/PledgeSupport.jsx
@@ -5,14 +5,14 @@ import PledgeSupportAmount from './PledgeSupportAmount.jsx'
 const PledgeSupport = (props) => (
     <div className='pledge-support-contaner' >
       {/* {console.log(props)} */}
-        <h1>Support</h1>
-        <div className='pledge-support-make-pledge-component'>
+      <h1>Support</h1>
+      <div className='pledge-support-make-pledge-component'>
 
-        </div>
+      </div>
 
-        {props.pledgeSupportAmounts.map((pledge, index) => (
-            <PledgeSupportAmount value={pledge} key={index} />
-        ))}
+      {props.pledgeSupportAmounts.map((pledge, index) => (
+          <PledgeSupportAmount value={pledge} key={index} />
+      ))}
 
     </div>
 );
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,4 @@ import PledgeInfo from './components/PledgeInfo.jsx'
 import PledgeSupport from './components/PledgeSupport.jsx';
   
 window.PledgeInfo = PledgeInfo;
-
-
 window.PledgeSupport = PledgeSupport;
