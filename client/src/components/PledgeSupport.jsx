bdc0c321604816cc2afb9b9c26b21f6476cd3227
James Reynolds <jameshreynolds@gmail.com>
2018-02-25 15:41:41 -0800

    refactored PledgeSupport component into a stateful component

--- client/src/components/PledgeSupport.jsx
+++ client/src/components/PledgeSupport.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import style from './../main.css';
 import PledgeSupportAmount from './PledgeSupportAmount.jsx'
 
-const PledgeSupport = (props) => (
-    <div className='pledge-support-contaner' >
-      {/* {console.log(props)} */}
-        <h1>Support</h1>
-        <div className='pledge-support-make-pledge-component'>
-
-        </div>
-
-        {props.pledgeSupportAmounts.map((pledge, index) => (
-            <PledgeSupportAmount value={pledge} key={index} />
-        ))}
-
-    </div>
-);
+class PledgeSupport extends React.Compnent {
+  constructor(props) {
+    super(props);
+    state = {};
+  }
+  
+  render() {
+    return (
+      <div className='pledge-support-contaner' >
+          <h1>Support</h1>
+          <div className='pledge-support-make-pledge-component'>
+  
+          </div>
+  
+          {props.pledgeSupportAmounts.map((pledge, index) => (
+              <PledgeSupportAmount value={pledge} key={index} />
+          ))}
+  
+      </div>
+    );
+  }
+}
 
 export default PledgeSupport;
