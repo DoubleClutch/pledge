8f22013f86de1c745f771100896b13728e2cd2fa
James Reynolds <jameshreynolds@gmail.com>
2018-02-25 12:42:25 -0800

    Refactored PledgeInfo component and index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,39 +1,7 @@
-import React from 'react';
-import {render} from 'react-dom';
-import StatefulComponent from './components/StatefulComponent.jsx';
-import StatelessComponent from './components/StatelessComponent.jsx';
 import PledgeInfo from './components/PledgeInfo.jsx'
-import style from './main.css';
 import PledgeSupport from './components/PledgeSupport.jsx';
-
-
-const date = new Date(); // temporary work around
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pledgeSupportAmounts: ['supportTest1', 'supportTest2'] // these will be replaced with objects containing necessary information
-    };
-  }
   
-  render () {
-    return (
-      <div>
-        <PledgeInfo info={{
-          pledgeTotal: '10,420',
-          pledgeGoal: '7,500',
-          backers: 352,
-          daysToGo: 18,
-          timeStamp: date.toString()
-        }}/>
-        <PledgeSupport pledgeSupportAmounts={this.state.pledgeSupportAmounts}/>
-      </div>
-    )
-  }
-}
+window.PledgeInfo = PledgeInfo;
 
-// export default App;
 
-render(<App/>, document.getElementById('pledge-info'));
+window.PledgeSupport = PledgeSupport;
