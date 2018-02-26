08484dde3f4cdf1c515337202e0dd66025240681
James Reynolds <jameshreynolds@gmail.com>
2018-02-25 17:31:41 -0800

    both PledgeSupport and PledgeInfo now up and running independently

--- client/src/components/PledgeSupport.jsx
+++ client/src/components/PledgeSupport.jsx
@@ -5,24 +5,39 @@ import PledgeSupportAmount from './PledgeSupportAmount.jsx'
 class PledgeSupport extends React.Component {
   constructor(props) {
     super(props);
-    state = {};
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.testUpdateState();
+  }
+
+  testUpdateState () {
+    setTimeout(() => {
+      this.setState({
+        pledgeSupportAmounts: ['test1', 'test2']
+      })
+    }, 250);
   }
   
   render() {
-    return (
-      <div className='pledge-support-contaner' >
-        <h1>Support</h1>
-        <div className='pledge-support-make-pledge-component'>
-
-
-      </div>
-
-        {props.pledgeSupportAmounts.map((pledge, index) => (
-            <PledgeSupportAmount value={pledge} key={index} />
-        ))}
+    if (this.state.pledgeSupportAmounts) {
+      return (
+        <div className='pledge-support-contaner' >
+          <h1>Support</h1>
   
-      </div>
-    );
+          <div className='pledge-support-make-pledge-component'>
+          </div>
+  
+          {this.state.pledgeSupportAmounts.map((pledge, index) => (
+              <PledgeSupportAmount value={pledge} key={index} />
+          ))}
+    
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
