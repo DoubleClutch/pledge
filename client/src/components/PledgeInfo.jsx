01243cc68c7f4f70d4533227ff6815c36fb79a43
James Reynolds <jameshreynolds@gmail.com>
2018-02-27 10:34:36 -0800

    css class rules working

--- client/src/components/PledgeInfo.jsx
+++ client/src/components/PledgeInfo.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import style from './../main.css';
+// import './../main.css';
 const date = new Date(); // temporary work around
 
-class PledgeInfo extends React.Component {
+class PledgeInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {}
@@ -28,14 +29,15 @@ class PledgeInfo extends React.Component {
   
   render() {
     if (this.state.pledgeInfo) {
+      console.log(style)
       return (
         <div className='pledge-info-container'>
-          <div><p className='pledge-info-number'><span>${this.state.pledgeInfo.pledgeTotal}</span></p></div>
-          <div><p className='pledge-info'>pledged of ${this.state.pledgeInfo.pledgeGoal} goal</p></div>
+          <div><p className="pledge-info-number"><span>${this.state.pledgeInfo.pledgeTotal}</span></p></div>
+          <div><p className={style.info}>pledged of ${this.state.pledgeInfo.pledgeGoal} goal</p></div>
           <div><p className='pledge-info-number'>{this.state.pledgeInfo.backers}</p></div>
-          <div><p className='pledge-info'>backers</p></div>
+          <div><p className={style.info}>backers</p></div>
           <div><p className='pledge-info-number'>{this.state.pledgeInfo.daysToGo}</p></div>
-          <div><p className='pledge-info'>days to go</p></div>
+          <div><p className={style.info}>days to go</p></div>
           <div><button className='pledge-info-project-btn' >Back this project</button></div>
           <div><button className='pledge-info-remind-btn' >Remind me</button></div>
           <div><p className='pledge-info-time-stamp'><span>All or nothing.</span> This project will only be funded if it reaches its goal by {this.state.pledgeInfo.timeStamp}</p></div>
