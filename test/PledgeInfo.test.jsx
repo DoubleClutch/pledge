e55269e8eee6094771601fdc6fe95cdfc8d2832f
James Reynolds <jameshreynolds@gmail.com>
2018-03-05 16:00:03 -0800

    basic test structure in place

--- test/PledgeInfo.test.jsx
+++ test/PledgeInfo.test.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { mount } from "enzyme";
 import PledgeInfo from './../client/src/components/PledgeInfo.jsx'
+import { shallow } from 'enzyme';
 
-describe('PledgeInfo', () => {
-  let props;
-  let id;
-  
+describe('PledgeInfo test', () => {
+  let wrapper;
+  beforeEach(() => { wrapper = shallow(<PledgeInfo />); });
+
+  it('inlcudes 1 div with class ".pledgeInfoContainer" when no props are passed', () => {
+    expect(wrapper.find('div.pledgeInfoContainer'));
+  });
 });
